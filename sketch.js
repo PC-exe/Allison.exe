2f1b191a93b3812abca06dd506af957fca930da7
PC-exe <paraschanana2005@gmail.com>
2024-07-21 17:23:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,12 +7,13 @@ let diam = 150;
 
 function draw() {
   fill(289, 60, 100)
+  //the circle will keep on make a stoke as the mouse goes
   circle(mouseX, mouseY, 150);
  // line(width/2, height/2, mouseX, mouseY);
   stroke('royalblue');
   strokeWeight(10);
 }
-
+//Click once to refresh the canvas and start over
 function mouseClicked() {
   background(225, 59, 60);
 }
